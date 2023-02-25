7bdc8f8931f39945032c32c1247b079edd129b47
hamza-crypto <6793sidique@gmail.com>
2023-02-25 22:57:38 +0500

    Just test added

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function () {
+    echo "This is just test page" . time();
 });
 
 Route::controller(WebhookController::class)->group(function () {
