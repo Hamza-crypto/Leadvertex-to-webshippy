b9614d8c47db9120dd719f2580b6d36905222b9b
hamza-crypto <6793sidique@gmail.com>
2023-02-27 22:55:42 +0500

    Products mapping added

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\EmailSchedule;
 use App\Models\EmailSchedules;
+use App\Models\ProductMapping;
+use App\Models\ProductWebhook;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -15,83 +17,100 @@ class WebhookController extends Controller
         $data = $request->all();
 
         app('log')->channel('webhooks')->info($data);
-        if ($data['status'] == 'accepted') {
+        if ($data['status'] != 'accepted') return;
 
-            $url = sprintf("%s/getOrdersByIds.html?token=%s&ids=%d", env('LEADVERTEX_API_URL'), env('TOKEN'), $data['id']);
 
-            $response = Http::get($url);
-            $response = json_decode($response);;
+        $url = sprintf("%s/getOrdersByIds.html?token=%s&ids=%d", env('LEADVERTEX_API_URL'), env('TOKEN'), $data['id']);
+
+        $response = Http::get($url);
+
+        ProductWebhook::create([
+            'product_id' => $data['id'],
+            'response' => $response
+        ]);
+
+        $response = json_decode($response);
 
 //            $json = file_get_contents(public_path('vertex.json'));
 //            $response = json_decode($json);
-            //just test push
 
-            foreach ($response as $order) {
-                $products = [];
-                foreach ($order->goods as $product) {
-                    $products[] = [
-                        'sku' => $product->goodID,
-                        'productName' => $product->name,
-                        'priceGross' => $product->price,
-                        'vat' => 0,
-                        'quantity' => $product->quantity
-                    ];
+
+        $quantity = false;
+        foreach ($response as $order) {
+            $products = [];
+            foreach ($order->goods as $product) {
+                $product_sku = ProductMapping::where('product_id_lv', $product->goodID)->first();
+                if(!$product_sku) continue;
+
+                $products[] = [
+                    'sku' => $product_sku->webshippy_sku,
+                    'productName' => $product->name,
+                    'priceGross' => $product->price,
+                    'vat' => 0,
+                    'quantity' => $product->quantity
+                ];
+                if ($product->quantity > 2 ){
+                    $quantity = true;
                 }
-
-                $request_body = json_encode([
-                    'apiKey' => env('TOKEN'),
-                    'order' => [
-                        'referenceId' => "LV#" . $data['id'],
-                        'createdAt' => $order->datetime,
-                        'shipping' => [
-                            'name' => $order->fio,
-                            'email' => $order->email,
-                            'phone' => $order->phone ?? "",
-                            'countryCode' => $order->country,
-                            'zip' => "",
-                            'city' => $order->city,
-                            'country' => $order->country,
-                            'address1' => $order->address,
-                            'note' => $order->comment
-                        ],
-                        'billing' => [
-                            'name' => $order->fio,
-                            'email' => $order->email,
-                            'phone' => $order->phone ?? "",
-                            'countryCode' => $order->country,
-                            'zip' => "",
-                            'city' => $order->city,
-                            'country' => $order->country,
-                            'address1' => $order->address
-
-                        ],
-                        'payment' => [
-                            'paymentMode' => "card",
-                            'codAmount' => 0,
-                            'paymentStatus' => "paid",
-                            'paidDate' => $order->lastUpdate,
-                            'shippingVat' => 0,
-                            'currency' => "HUF",
-                            'discount' => 0
-                        ],
-                        'products' => $products
-                    ]
-                ]);
-
-                $url = sprintf("%s/CreateOrder/json", env('WEBSHIPPY_API_URL'));
-                $request_body = ['request' => $request_body];
-
-                $response = Http::withHeaders([
-                    'Content-Type' => 'application/x-www-form-urlencoded'
-                ])->asForm()->post($url, $request_body);
-                app('log')->channel('webhooks')->info($response->json());
-
             }
+
+            $request_body = [
+                'apiKey' => env('TOKEN'),
+                'order' => [
+                    'referenceId' => "LV#" . $data['id'],
+                    'createdAt' => $order->datetime,
+                    'shipping' => [
+                        'name' => $order->fio,
+                        'email' => $order->email,
+                        'phone' => $order->phone ?? "",
+                        'countryCode' => $order->country,
+                        'zip' => "",
+                        'city' => $order->city,
+                        'country' => $order->country,
+                        'address1' => $order->address,
+                        'note' => $order->comment
+                    ],
+                    'billing' => [
+                        'name' => $order->fio,
+                        'email' => $order->email,
+                        'phone' => $order->phone ?? "",
+                        'countryCode' => $order->country,
+                        'zip' => "",
+                        'city' => $order->city,
+                        'country' => $order->country,
+                        'address1' => $order->address
+
+                    ],
+                    'payment' => [
+                        'paymentMode' => "card",
+                        'codAmount' => 0,
+                        'paymentStatus' => "paid",
+                        'paidDate' => $order->lastUpdate,
+                        "shippingPrice" => 1500,
+                        'shippingVat' => 0,
+                        'currency' => "HUF",
+                        'discount' => 0
+                    ],
+                    'products' => $products
+                ]
+            ];
+
+            if ($quantity) {
+                $request_body['order']['payment']['shippingPrice'] = 0;
+            }
+
+            $url = sprintf("%s/CreateOrder/json", env('WEBSHIPPY_API_URL'));
+            $request_body = ['request' => json_encode($request_body)];
+
+
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/x-www-form-urlencoded'
+            ])->asForm()->post($url, $request_body);
+            app('log')->channel('webhooks')->info($response->json());
 
         }
 
-
-    }
+        }
 
 
 }
