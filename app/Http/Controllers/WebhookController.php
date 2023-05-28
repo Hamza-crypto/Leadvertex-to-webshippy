f79e79b155125d898ec3e8701a3a3c8be1a5c3af
hamza-crypto <6793sidique@gmail.com>
2023-05-28 23:14:03 +0500

    Error Validations

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -148,13 +148,13 @@ class WebhookController extends Controller
     public function sendData($name, $phone, $productName, $id, $date){
 
         //If starting from 0, then append 3 at the begining
-        // if (strlen($phone) > 11) {
-        //     $phone = substr($phone, -11);
-        // }
+        if (strlen($phone) > 11) {
+            $phone = substr($phone, -11);
+        }
 
-        // if (substr($phone, 0, 1) === "0") {
-        //     $phone = "3" . substr($phone, 1);
-        // }
+        if (substr($phone, 0, 1) === "0") {
+            $phone = "3" . substr($phone, 1);
+        }
 
         $data = [
             'rq_sent' => '',
@@ -212,7 +212,6 @@ class WebhookController extends Controller
             $result = "Comnica ID: ";
             $result .= $main_response->payload->id;
         }
-
 
         DiscordAlert::message($result);
 
