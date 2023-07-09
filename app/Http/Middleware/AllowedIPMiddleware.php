64688c51ce2aaa8d80718be874e12e5fee9befb6
Hamza <6793siddique@gmail.com>
2023-07-09 16:45:47 +0500

    Added ip based middleware to Telescope

--- app/Http/Middleware/AllowedIPMiddleware.php
+++ app/Http/Middleware/AllowedIPMiddleware.php
@@ -7,13 +7,25 @@ use Illuminate\Http\Request;
 
 class AllowedIPMiddleware
 {
-    public $allowed_ips = ['124.109.46.230', '119.160.20.190'];
+    public $allowed_ips = ['124.109.46.230', '119.160'];
 
     public function handle(Request $request, Closure $next)
     {
-        if (!in_array($request->ip(), $this->allowed_ips)) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+        $ip = $request->ip();
+        $ipFragments = explode('.', $ip);
+
+        // Check if the IP address matches the allowed IPs
+        foreach ($this->allowed_ips as $allowedIp) {
+            $allowedIpFragments = explode('.', $allowedIp);
+
+            // Compare the first two fragments
+            if ($ipFragments[0] == $allowedIpFragments[0] && $ipFragments[1] == $allowedIpFragments[1]) {
+                return $next($request);
+            }
         }
-        return $next($request);
+
+        // IP address is not allowed
+        return response()->json(['message' => 'Unauthorized'], 401);
     }
 }
+
