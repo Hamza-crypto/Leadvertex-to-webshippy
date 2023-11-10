b48a9c64ebbed6e200631fde9c94e815f6bb1a38
Hamza <6793siddique@gmail.com>
2023-11-10 17:54:21 +0500

    DarkLEad api

--- app/Http/Controllers/DarkLeadsController.php
+++ app/Http/Controllers/DarkLeadsController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class DarkLeadsController extends Controller
 {
-    public function add_new_order(Request $request){
-
+    public function add_new_order(Request $request)
+    {
         $url = sprintf("%s/addOrder.html?token=%s", env('LEADVERTEX_API_URL'), env('TOKEN'));
 
         $request_body = [
@@ -30,11 +30,48 @@ class DarkLeadsController extends Controller
             'Content-Type' => 'application/x-www-form-urlencoded',
         ])->asForm()->post($url, $request_body);
 
-        return $lv_response->json();
+        app('log')->channel('darkleads')->info($lv_response->json());
+
+        $statusCode = $lv_response->status();
+        $responseJson = $lv_response->json();
+
+        if ($statusCode == 200) {
+            $responseJson = $lv_response->json();
+
+            $dynamicId = key($responseJson);
+
+            $response = [
+                'result' => 'success',
+                'id' => $dynamicId,
+            ];
+
+        } else {
+
+            $response = [
+                'result' => 'failed',
+                'error' => $responseJson['error'] ?? 'Unknown Error',
+                'code' => $responseJson['code'] ?? null,
+            ];
+
+        }
+
+        return response()->json($response, $statusCode);
     }
 
-    public function send_status_update(Request $request)
-    {
+    public function send_status_update(Request $request) {
 
+        if ($status == 'accepted') {
+            $arknetStatus = 'approve';
+        }
+        elseif ($status == 'spam') {
+            $arknetStatus = 'trash';
+        }
+        else{
+            $arknetStatus = 'reject';
+        }
+
+        $darklead_url = sprintf("%s?barcode=%s&subid=%s&status=%s", env('DARKLEADS_BASE_URL'), env('DARKLEADS_BARCODE'), $subid, $arknetStatus);
+
+        Http::get($darklead_url);
     }
 }
