d18ac1e3aa2811849b90a5bf7083668a03262802
Hamza <6793siddique@gmail.com>
2024-01-20 10:50:48 +0500

    Dont check csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,6 +16,7 @@ class VerifyCsrfToken extends Middleware
         'leadvertex-all-orders/webhook',
         'orders',
         'naturprime_leadvertex_new_order/webhook',
-        'zapier_fb_lead'
+        'zapier_fb_lead',
+        '/facebook/webhook'
     ];
 }
