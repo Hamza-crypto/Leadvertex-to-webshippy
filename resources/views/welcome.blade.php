48e16097de59edb6715a71822688833d193952f4
Hamza <6793siddique@gmail.com>
2024-02-15 11:23:37 +0500

    Create beautiful page for bulk update status

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,58 @@
-<h1 class="h3 mb-3">Add New File </h1>
+@extends('layouts.app')
 
-<div class="row">
+@section('title', 'Add File')
 
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <form method="post" action="{{ route('status.bulk.update') }}" enctype="multipart/form-data">
-                    @csrf
+@section('scripts')
+    <script></script>
+@endsection
 
-                    <div class="form-group">
-                        <div class="mb-3">
-                            <label class="form-label w-100">Copart</label>
-                            <input type="file" name="csv_file" required>
+@section('content')
+
+    <h1 class="h3 mb-3">Bulk Status Update</h1>
+
+    <div class="row">
+
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <form method="post" action="{{ route('status.bulk.update') }}" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="form-group">
+                            <div class="mb-3">
+                                <label class="form-label w-100">CSV File</label>
+                                <input type="file" name="file" required>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-lg btn-primary add-btn">Add New File
-                        </button>
-                    </div>
+                        <div class="form-group">
+                            <label class="form-label w-100">Select Status</label>
+                            <div class="mb-3">
+                                <label class="form-check">
+                                    <input name="status" type="radio" class="form-check-input" value="paid"
+                                        checked="">
+                                    <span class="form-check-label">PAID</span>
+                                </label>
+                                <label class="form-check">
+                                    <input name="status" type="radio" class="form-check-input" value="refused">
+                                    <span class="form-check-label">RETURN</span>
+                                </label>
+                            </div>
+                        </div>
 
 
-                </form>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-lg btn-primary add-btn">Upload
+                            </button>
+                        </div>
+
+
+                    </form>
+                </div>
             </div>
         </div>
+
     </div>
 
-</div>
+
+@endsection
