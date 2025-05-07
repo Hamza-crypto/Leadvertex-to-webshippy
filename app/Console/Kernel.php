0ce60d1bf28baf5e8d1543817d63aeed2d909805
Hamza Siddique <6793siddique@gmail.com>
2025-05-07 22:22:20 +0500

    fix: update order sending schedule to run daily at 10:00 instead of every six hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('check:webshippy-order-status')->everyMinute();
         $schedule->command('telescope:prune --hours=668')->daily();
-        $schedule->command('orders:send-tomorrow')->everySixHours();
+        $schedule->command('orders:send-tomorrow')->dailyAt('10:00');
     }
 
     /**
